fb514b8ad875dd65339f1b0cb9ef34ada91cb746
John Hume <duelin.markers@gmail.com>
2014-10-14 00:12:03 -0500

    Work in progress.

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject duelinmarkers/life-of-a-clojure-expression "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.analyzer.jvm "0.6.1"]])
---
+++ src/a_map.clj
@@ -0,0 +1,8 @@
+(ns a-map)
+
+(defn m [v] {:foo "bar" :baz v})
+
+(def a-const {:foo "bar" :baz 23})
+
+(defn m-big [v]
+  {:a 1 :b 2 :c 3 :d 4 :e 5 :f 6 :g 7 :h 8 :i v})
