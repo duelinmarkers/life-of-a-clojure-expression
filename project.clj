b41fe4f3cf48ed42f7d6a174e1d3e11f114850e1
John Hume <duelin.markers@gmail.com>
2014-11-09 22:01:21 -0600

    Use my locally built clojure.

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject duelinmarkers/life-of-a-clojure-expression "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]
                  [org.clojure/tools.analyzer.jvm "0.6.1"]])
