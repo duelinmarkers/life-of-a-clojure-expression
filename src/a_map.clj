b10c96657dd7d751210444d9736d3867cdcbc54e
John Hume <duelin.markers@gmail.com>
2014-11-09 22:02:18 -0600

    More simple example code to inspect.

--- src/a_map.clj
+++ src/a_map.clj
@@ -4,5 +4,9 @@
 
 (def a-const {:foo "bar" :baz 23})
 
+(defn a-const-fn [] {:foo "bar" :baz 24})
+
 (defn m-big [v]
   {:a 1 :b 2 :c 3 :d 4 :e 5 :f 6 :g 7 :h 8 :i v})
+
+(defn calls-m [] (m "my val"))
