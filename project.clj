784b3a6147e6fff50bf311ad51a017fdd7ef9d8c
John Hume <duelin.markers@gmail.com>
2015-03-22 17:31:22 -0500

    Upgrade clojure to 1.7.0-alpha5.

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject duelinmarkers/life-of-a-clojure-expression "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [org.clojure/tools.analyzer.jvm "0.6.1"]])
