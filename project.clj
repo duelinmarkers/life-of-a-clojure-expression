b951a5d06aba2f6b3b195298cbff705847f4db0c
John Hume <duelin.markers@gmail.com>
2015-04-03 15:29:05 -0500

    aot all for javap fun.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject duelinmarkers/life-of-a-clojure-expression "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                 [org.clojure/tools.analyzer.jvm "0.6.1"]])
+                 [org.clojure/tools.analyzer.jvm "0.6.1"]]
+  :aot :all)
